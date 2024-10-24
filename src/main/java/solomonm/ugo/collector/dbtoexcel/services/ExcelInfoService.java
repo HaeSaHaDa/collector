bf47148bb3e21ab0c21986c3d44201953c1fc740
sixthgkrsus <sixthgkrsus@naver.com>
2024-10-24 11:00:37 +0900

    data select부분까지.

---
+++ src/main/java/solomonm/ugo/collector/dbtoexcel/services/ExcelInfoService.java
@@ -0,0 +1,12 @@
+package solomonm.ugo.collector.dbtoexcel.services;
+
+import org.springframework.stereotype.Service;
+import solomonm.ugo.collector.dbtoexcel.dto.DbinfoDTO;
+import solomonm.ugo.collector.dbtoexcel.dto.ExcelColDTO;
+import solomonm.ugo.collector.dbtoexcel.mapper.ExcelInfoMapper;
+
+import java.util.List;
+
+public interface ExcelInfoService {
+    List<ExcelColDTO> selectData();
+}
